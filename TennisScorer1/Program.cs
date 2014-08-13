2bdd8b26bad9dc073ad1aa53d31e86a7d2a29210
unknown <a0132687x@CR3515.iss.nus.edu.sg>
2014-08-13 14:00:42 +0800

    My student number is a0132687x.

--- TennisScorer1/Program.cs
+++ TennisScorer1/Program.cs
@@ -20,10 +20,185 @@ namespace TennisScorer1
             Console.ReadLine();
         }
 
-        static void DisplayScore(int a, int b)
+        static void DisplayScore(int fscore, int sscore)
         {
             //Modify this method - use ifs and switches based on the value of a and b
-            Console.WriteLine("____");
+            //Console.WriteLine("____");
+            switch (fscore)
+            {
+                case 0:
+                    DisplayforZero(sscore);
+                    break;
+                case 1:
+                    DisplayforOne(sscore);
+                    break;
+                case 2:
+                    DisplayforTwo(sscore);
+                    break;
+                case 3:
+                    DisplayforThree(sscore);
+                    break;
+                case 4:
+                    DisplayforFour(sscore);
+                    break;
+                default: DisplayforFive(sscore);
+                    break;
+            }
+        }
+
+        static void DisplayforZero(int sscore)
+        {
+            switch (sscore)
+            {
+                case 0:
+                        Console.WriteLine("Love all");
+                        break;
+                case 1:
+                        Console.WriteLine("Love-15");
+                        break;
+                case 2:
+                        Console.WriteLine("Love-30");
+                        break;
+                case 3   :
+                        Console.WriteLine("Love-40");
+                        break;
+                case 4:
+                        Console.WriteLine("Game B");
+                        break;
+                default:
+                        Console.WriteLine("N/A");
+                    break;
+
+            }
+        }
+
+        static void DisplayforOne(int sscore)
+        {
+            switch (sscore)
+            {
+                case 0:
+                    Console.WriteLine("15-love");
+                    break;
+                case 1:
+                    Console.WriteLine("15 all");
+                    break;
+                case 2:
+                    Console.WriteLine("15 30");
+                    break;
+                case 3:
+                    Console.WriteLine("15 40");
+                    break;
+                case 4:
+                    Console.WriteLine("Game B");
+                    break;
+                default:
+                    Console.WriteLine("N/A");
+                    break;
+
+            }
+        }
+
+        static void DisplayforTwo(int sscore)
+        {
+            switch (sscore)
+            {
+                case 0:
+                    Console.WriteLine("30-love");
+                    break;
+                case 1:
+                    Console.WriteLine("30 15");
+                    break;
+                case 2:
+                    Console.WriteLine("30 all");
+                    break;
+                case 3:
+                    Console.WriteLine("30 40");
+                    break;
+                case 4:
+                    Console.WriteLine("Game B");
+                    break;
+                default:
+                    Console.WriteLine("N/A");
+                    break;
+
+            }
+        }
+
+        static void DisplayforThree(int sscore)
+        {
+            switch (sscore)
+            {
+                case 0:
+                    Console.WriteLine("40-love");
+                    break;
+                case 1:
+                    Console.WriteLine("40 15");
+                    break;
+                case 2:
+                    Console.WriteLine("40 30");
+                    break;
+                case 3:
+                    Console.WriteLine("Deuce");
+                    break;
+                case 4:
+                    Console.WriteLine("Advantage B");
+                    break;
+                default:
+                    Console.WriteLine("Game B");
+                    break;
+
+            }
+        }
+         static void DisplayforFour(int sscore)
+        {
+            switch (sscore)
+            {
+                case 0:
+                    Console.WriteLine("Game A");
+                    break;
+                case 1:
+                    Console.WriteLine("Game A");
+                    break;
+                case 2:
+                    Console.WriteLine("Game A");
+                    break;
+                case 3:
+                    Console.WriteLine("Advantage A");
+                    break;
+                case 4:
+                    Console.WriteLine("Deuce");
+                    break;
+                default:
+                    Console.WriteLine("Advantage B");
+                    break;
+
+            }
+        }
+
+         static void DisplayforFive(int sscore)
+        {
+            switch (sscore)
+            {
+                case 0:
+                    Console.WriteLine("N/A");
+                    break;
+                case 1:
+                    Console.WriteLine("N/A");
+                    break;
+                case 2:
+                    Console.WriteLine("N/A");
+                    break;
+                case 3:
+                    Console.WriteLine("Game A");
+                    break;
+                case 4:
+                    Console.WriteLine("Advantage A");
+                    break;
+                default:
+                    Console.WriteLine("Deuce");
+                    break;
+
+            }
         }
     }
 }
